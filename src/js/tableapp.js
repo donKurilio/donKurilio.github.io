754ce88419314dd21763bdb4af57cd05f4a40db6
donKurilio <adskiyyo@mail.ru>
2016-06-02 17:31:35 +0300

    fix savePerson method

--- src/js/tableapp.js
+++ src/js/tableapp.js
@@ -13,15 +13,15 @@ angular.module("TableApp", ["ngResource"])
             $scope.showModal = true;
         };
         $scope.savePerson = function (id) {
-            if (id) {
+            if (id!==undefined) {
                 $scope.persons = _.map($scope.persons, function (obj) {
-                    return obj.id == id ? $scope.bufPerson : obj;
+                    return obj.id === id ? $scope.bufPerson : obj;
                 })
             }
             else {
                 $scope.bufPerson.id = _.max(_.pluck($scope.persons, "id")) + 1;
+                $scope.persons.push($scope.bufPerson);
             }
-            $scope.persons.push($scope.bufPerson);
             $scope.showModal = false;
         };
         $scope.addPerson = function () {
